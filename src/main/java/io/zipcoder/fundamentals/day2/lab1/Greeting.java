158e80175ddc7c4646c8a2824fcfd4ce37cc5b3c
Tegan Salter-George <tsaltergeorge@hotmail.com>
2017-08-10 10:32:58 +0100

    Finish lab

--- src/main/java/io/zipcoder/fundamentals/day2/lab1/Greeting.java
+++ src/main/java/io/zipcoder/fundamentals/day2/lab1/Greeting.java
@@ -1,9 +1,30 @@
 package io.zipcoder.fundamentals.day2.lab1;
-
+import java.util.*;
 
 public class Greeting {
 
-    public static void main(String[] args){
+    public static void main(String[] args)
+    {
+    	Greeting greeting = new Greeting();
+    	greeting.askName();
 
+    }
+    
+    public void askName()
+    {
+    	Scanner in = new Scanner(System.in);
+    	
+    	System.out.print("Hello, what is your name?");
+    	String name = in.nextLine();
+    	
+    	System.out.print("Oh, heeeyyyy ");
+    	if(name.equals("Alice") || name.equals("Alice"))
+    	{
+    		System.out.print(name);
+
+    	}
+    	
+
+    	in.close();      
     }
 }
